afb5f9ccf4961c8104040f67ec9230c7011d63aa
Celestinvib <celestinvib@gmail.com>
2022-07-08 10:25:00 +0200

    Ejercicio 3 finalizado

---
+++ TA06/src/TresApp.java
@@ -0,0 +1,26 @@
+import javax.swing.JOptionPane;
+
+public class TresApp {
+	public static boolean primo = true;
+
+	public static void main(String[] args) {
+		
+		int num = Integer.parseInt(JOptionPane.showInputDialog(null,"Introduce un número para averiguar si es primo:"));
+		  primo = esPrimo(num); 
+		  if(primo) {
+			  JOptionPane.showMessageDialog(null,num+ " es un número primo");
+		  }else {
+			  JOptionPane.showMessageDialog(null,num+ " no es un número primo");
+		  }
+	}
+	
+	public static boolean esPrimo (int num) {
+		for (int i = num; i > 0 ; i--) {
+			if(i != num && i != 1 && num % i == 0) {
+				primo = false;
+				break;
+			}
+		}	
+		return primo;
+	}
+}
